a8d36d75f4002b6273e898c4d8964bac2d049bb5
am-gforce <47460424+am-gforcehub@users.noreply.github.com>
2019-04-02 15:56:56 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,42 @@
 $(document).ready(function () {
 
-
+    var options = [
+        {
+            question: "What is the resolution of the human eye?",
+            choice: ["576 megapixels", "820 megapixels", "1024 megapixels", "600 megapixels"],
+            answer: 0,
+            photo: "assets/images/eye-2771174_1920.jpg"
+        },
+        {
+            question: "What was the first planet to be discovered using the telescope, in 1781?",
+            choice: ["Earth", "Mercury", "Jupitar", "Uranus"],
+            answer: 3,
+            photo: "assets/images/solar-system-439046_1920.jpg"
+        },
+        {
+            question: "What is the resolution of the human eye?",
+            choice: ["576 megapixels", "820 megapixels", "1024 megapixels", "600 megapixels"],
+            answer: 0,
+            photo: "assets/images/eye-2771174_1920.jpg"
+        },
+        {
+            question: "What is the resolution of the human eye?",
+            choice: ["576 megapixels", "820 megapixels", "1024 megapixels", "600 megapixels"],
+            answer: 0,
+            photo: "assets/images/eye-2771174_1920.jpg"
+        },
+        {
+            question: "What is the resolution of the human eye?",
+            choice: ["576 megapixels", "820 megapixels", "1024 megapixels", "600 megapixels"],
+            answer: 0,
+            photo: "assets/images/eye-2771174_1920.jpg"
+        },
+        {
+            question: "What is the resolution of the human eye?",
+            choice: ["576 megapixels", "820 megapixels", "1024 megapixels", "600 megapixels"],
+            answer: 0,
+            photo: "assets/images/eye-2771174_1920.jpg"
+        },
 
 
 
